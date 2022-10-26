625a45f3baa2d4748d7b9ddcc16c71162529777d
Curtis Wilson <cj@wilsontech.com>
2022-10-25 23:15:51 -0700

    Add initial actix_web server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, web, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("Server running at http://localhost:{}/", PORT);
+
+    HttpServer::new(move || {
+        App::new()
+            .service(web::resource("/").to(index))
+    })
+    .bind(("127.0.0.1", PORT))?
+    .run()
+    .await
 }
+
+async fn index() -> impl Responder {
+    "Hello World!"
+}
+
+const PORT: u16 = 8000;
